Bug fixes with attention mask computation

dtrocr/model.py
@@ -21,14 +21,16 @@ class DTrOCR(nn.Module):
         self.layer_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_epsilon)
         self.position_ids = torch.arange(config.max_position_embeddings, dtype=torch.long).unsqueeze(0)
 
-        # initialise weights from GPT-2
+        self._attn_implementation = config._attn_implementation
+
+        # initialise GPT-2 weights from Hugging Face
         self.initialise_weights(config)
 
     def forward(
         self,
         pixel_values: torch.Tensor,
         input_ids: torch.LongTensor,
-        attention_mask: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None
     ):
         input_ids = input_ids.view(-1, input_ids.shape[-1])
 
@@ -44,20 +46,34 @@ class DTrOCR(nn.Module):
 
         # attention mask
         if attention_mask is not None:
-            attention_mask = attention_mask.view(input_shape[0], -1)
-            if self._attn_implementation == "flash_attention_2":
-                attention_mask = attention_mask if 0 in attention_mask else None
-            elif self._attn_implementation == "sdpa":
+            attention_mask = torch.concat(
+                [
+                    torch.ones(attention_mask.shape[0], patch_embeddings.shape[-2], dtype=attention_mask.dtype),
+                    attention_mask
+                ], dim=-1
+            )
+            if self._attn_implementation == "sdpa":
                 attention_mask = _prepare_4d_causal_attention_mask_for_sdpa(
                     attention_mask=attention_mask,
-                    input_shape=(input_shape[0], input_shape[-1]),
+                    input_shape=(input_shape[0], input_shape[-2]),
                     inputs_embeds=patch_and_token_embeddings,
                     past_key_values_length=0,
                 )
             else:
+                # We create a 3D attention mask from a 2D tensor mask.
+                # Sizes are [batch_size, 1, 1, to_seq_length]
+                # So we can broadcast to [batch_size, num_heads, from_seq_length, to_seq_length]
+                # this attention mask is more simple than the triangular masking of causal attention
+                # used in OpenAI GPT, we just need to prepare the broadcast dimension here.
                 attention_mask = attention_mask[:, None, None, :]
-                attention_mask = attention_mask.to(dtype=self.dtype)  # fp16 compatibility
-                attention_mask = (1.0 - attention_mask) * torch.finfo(self.dtype).min
+
+                # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+                # masked positions, this operation will create a tensor which is 0.0 for
+                # positions we want to attend and the dtype's smallest value for masked positions.
+                # Since we are adding it to the raw scores before the softmax, this is
+                # effectively the same as removing these entirely.
+                # attention_mask = attention_mask.to(dtype=self.dtype)  # fp16 compatibility
+                attention_mask = (1.0 - attention_mask) * torch.finfo(torch.float32).min
 
         for hidden_layer in self.hidden_layers:
             outputs = hidden_layer(hidden_states, attention_mask=attention_mask)