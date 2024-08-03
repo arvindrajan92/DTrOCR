dedb03226903c5dbba8a24addd4e3f20a979a812
Arvind Rajan <arvindrajan92@gmail.com>
2024-08-03 12:13:23 +1000

    DTrOCR-18 - Updating test cases

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,75 +8,68 @@ import random
 from PIL import Image
 from dataclasses import asdict
 
+# global variables
+BATCH_SIZE = random.choice(range(1, 10))
+BEAM_SIZE = random.choice(range(1, 3))
+CONFIG = DTrOCRConfig()
+MODEL = DTrOCRLMHeadModel(CONFIG)
+
+# set model to evaluation mode
+MODEL.eval()
+
 
 def test_model():
-    batch_size = random.choice(range(1, 10))
-
-    config = DTrOCRConfig()
-    model = DTrOCRLMHeadModel(config)
-    processor = DTrOCRProcessor(config=config, add_bos_token=True, add_eos_token=True)
+    processor = DTrOCRProcessor(config=CONFIG, add_bos_token=True, add_eos_token=True)
 
     inputs = processor(
-        images=[Image.new("RGB", config.image_size[::-1]) for _ in range(batch_size)],
-        texts=["This is a sentence" for _ in range(batch_size)],
+        images=[Image.new("RGB", CONFIG.image_size[::-1]) for _ in range(BATCH_SIZE)],
+        texts=["This is a sentence" for _ in range(BATCH_SIZE)],
         padding=True,
         return_tensors="pt",
         return_labels=True
     )
 
-    model_output = model(**asdict(inputs))
+    model_output = MODEL(**asdict(inputs))
 
     assert model_output.loss.shape == ()
     assert model_output.accuracy.shape == ()
     assert model_output.logits.shape == (
-        batch_size,
-        int(((config.image_size[0] / config.patch_size[0]) * (config.image_size[1] / config.patch_size[1]))) +
+        BATCH_SIZE,
+        int(((CONFIG.image_size[0] / CONFIG.patch_size[0]) * (CONFIG.image_size[1] / CONFIG.patch_size[1]))) +
         inputs.attention_mask.shape[1],
-        config.vocab_size
+        CONFIG.vocab_size
     )
 
 
 def test_generation_to_be_deterministic():
-    beam_size = random.choice(range(1, 3))
-
-    config = DTrOCRConfig()
-    model = DTrOCRLMHeadModel(config)
     processor = DTrOCRProcessor(DTrOCRConfig())
 
     inputs = processor(
-        images=Image.new("RGB", config.image_size[::-1]),
-        texts=processor.tokeniser.bos_token,
+        images=[Image.new("RGB", CONFIG.image_size[::-1]) for _ in range(BATCH_SIZE)],
+        texts=[processor.tokeniser.bos_token for _ in range(BATCH_SIZE)],
         return_tensors="pt"
     )
 
-    model.eval()
-
-    output_1 = model.generate(inputs=inputs, processor=processor, num_beams=beam_size, use_cache=False)
-    output_2 = model.generate(inputs=inputs, processor=processor, num_beams=beam_size, use_cache=False)
+    output_1 = MODEL.generate(inputs=inputs, processor=processor, num_beams=BEAM_SIZE, use_cache=False)
+    output_2 = MODEL.generate(inputs=inputs, processor=processor, num_beams=BEAM_SIZE, use_cache=False)
     assert torch.equal(output_1, output_2)
 
 
 def test_generation_with_and_without_caching():
-    beam_size = random.choice(range(1, 3))
-
-    config = DTrOCRConfig()
-    model = DTrOCRLMHeadModel(config)
     processor = DTrOCRProcessor(DTrOCRConfig())
 
     inputs = processor(
-        images=Image.new("RGB", config.image_size[::-1]),
-        texts=processor.tokeniser.bos_token,
+        images=[Image.new("RGB", CONFIG.image_size[::-1]) for _ in range(BATCH_SIZE)],
+        texts=[processor.tokeniser.bos_token for _ in range(BATCH_SIZE)],
         return_tensors="pt"
     )
 
-    model.eval()
-
     start_time = time.time()
-    output_without_cache = model.generate(inputs=inputs, processor=processor, num_beams=beam_size, use_cache=False)
+    output_without_cache = MODEL.generate(inputs=inputs, processor=processor, num_beams=BEAM_SIZE, use_cache=False)
     time_without_cache = time.time() - start_time
 
     start_time = time.time()
-    output_with_cache = model.generate(inputs=inputs, processor=processor, num_beams=beam_size, use_cache=True)
+    output_with_cache = MODEL.generate(inputs=inputs, processor=processor, num_beams=BEAM_SIZE, use_cache=True)
     time_with_cache = time.time() - start_time
 
     assert torch.equal(output_without_cache, output_with_cache)
